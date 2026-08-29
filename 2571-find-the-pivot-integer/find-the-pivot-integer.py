b626cb0c247a60c58fcd571f6a299eaf70179f62
Dhamodharan-122006 <dhamodharankh@gmail.com>
2026-08-29 20:11:45 +0530

    Time: 591 ms (16.67%) | Memory: 19.4 MB (22.53%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -0,0 +1,10 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        nums = [i for i in range(1,n+1)]
+        print(nums)
+        for i in range(len(nums)):
+            if sum(nums[:i+1]) == sum(nums[i:]):
+                return nums[i]
+        return -1
+        
+        
