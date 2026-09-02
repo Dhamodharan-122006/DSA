593dba8cdd25671ad8a350c92db25cdcd66b8e10
DHAMODHARAN_K_H <dhamodharankh@gmail.com>
2026-09-02 19:13:07 +0530

    Time: 7 ms (14.83%) | Memory: 19.8 MB (26.57%) - LeetSync

---
+++ 1184-car-pooling/car-pooling.py
@@ -0,0 +1,19 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        prefix = [0]*(1001)
+        for n,f,t in trips:
+            prefix[f] += n
+            prefix[t] -= n
+        print(prefix)
+
+        res = []
+        ans = 0
+        for num in prefix:
+            ans += num
+            res.append(ans)
+        print(res)
+        maxi = max(res)
+        if maxi <= capacity:
+            return True
+        else:
+            return False
