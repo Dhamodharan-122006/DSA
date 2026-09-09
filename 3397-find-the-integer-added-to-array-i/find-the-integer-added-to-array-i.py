e7531b79dd70d21c3d58dd8a750cd2cfe2c5d8f9
DHAMODHARAN_K_H <dhamodharankh@gmail.com>
2026-09-09 20:41:07 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (58.03%) - LeetSync

--- 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.py
@@ -3,7 +3,5 @@ class Solution:
         nums1.sort()
         nums2.sort()
         diff = nums2[0] - nums1[0]
-        for i in range(len(nums1)):
-            nums1[i] = nums1[i] + diff
         return diff
         
