ed9eb14a99ceb7d0aef36f129bcc90e3b343c57e
DHAMODHARAN_K_H <dhamodharankh@gmail.com>
2026-09-19 21:09:38 +0530

    Time: 551 ms (6.75%) | Memory: 19.3 MB (38.52%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.py
@@ -4,7 +4,6 @@ class Solution:
         for i in range(num1,num2+1):
             n = str(i)
             num = list(n)
-            print(num)
             for j in range(1,len(num)-1):
                 if int(num[j-1]) < int(num[j]) and int(num[j]) > int(num[j+1]):
                     s += 1
